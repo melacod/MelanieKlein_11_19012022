47cb281ca000eff0cb846115338ccbd2b37961e2
melacod <melaniebordeaux2015@gmail.com>
2022-01-29 18:22:30 +0100

    intégration du composant thumbs container dans home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,33 @@
-import ThumbsContainer from '../components/ThumbsContainer.js'
+import { useFetch } from '../utils/hooks.js'
 
 import imageTop from '../assets/top-home.jpg'
+
+import Error from '../components/Error.js'
+import Loader from '../components/Loader.js'
+import Thumb from '../components/Thumb.js'
 
 import './Home.css'
 
 export default function Home() {
+    const { loading, data, error, exception } = useFetch('data/locations.json')
+
     return (
         <div className="home">
             <div className="top">
                 <img src={imageTop} alt="Beautiful landscape" />
                 <div>Chez vous, partout et ailleurs</div>
             </div>
-            <ThumbsContainer />
+            <div className="thumbs-container">
+                {error ? (
+                    <Error exception={exception} />
+                ) : loading ? (
+                    <Loader />
+                ) : (
+                    data.map((logement) => (
+                        <Thumb key={logement.id} logement={logement} />
+                    ))
+                )}
+            </div>
         </div>
     )
 }
