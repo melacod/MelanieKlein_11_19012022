b23533204cbb7bece8ececdc819f2ba7e044a6af
melacod <melaniebordeaux2015@gmail.com>
2022-02-08 09:28:16 +0100

    location rendu pour mobile

--- src/assets/Star.js
+++ src/assets/Star.js
@@ -5,8 +5,6 @@ export default function Star({ active = false }) {
         <svg
             className="star"
             fill={color}
-            width="30"
-            height="30"
             viewBox="0 0 30 30"
             xmlns="http://www.w3.org/2000/svg"
         >
