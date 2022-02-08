9fa6e585d268a15ab97241c12dcf45cd84baaf47
melacod <melaniebordeaux2015@gmail.com>
2022-02-08 08:59:26 +0100

    mobile pour gallery

--- src/assets/ArrowLeft.js
+++ src/assets/ArrowLeft.js
@@ -1,9 +1,18 @@
-export default function ArrowLeft({ color = 'current', onClickFunction = undefined }) {
-    
+export default function ArrowLeft({
+    color = 'current',
+    onClickFunction = undefined,
+}) {
     return (
-        <svg className="arrow" width="48" height="80" onClick={onClickFunction} viewBox="0 0 48 80" fill={color} xmlns="http://www.w3.org/2000/svg">
-            <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill={color} />
+        <svg
+            className="arrow"
+            onClick={onClickFunction}
+            fill={color}
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z"
+                fill={color}
+            />
         </svg>
     )
-
 }
--- src/assets/ArrowRight.js
+++ src/assets/ArrowRight.js
@@ -1,9 +1,18 @@
-export default function ArrowRight({ color = 'current', onClickFunction = undefined }) {
-
+export default function ArrowRight({
+    color = 'current',
+    onClickFunction = undefined,
+}) {
     return (
-        <svg className="arrow" width="48" height="80" viewBox="0 0 48 80" onClick={onClickFunction} fill={color} xmlns="http://www.w3.org/2000/svg">
-            <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill={color} />
+        <svg
+            className="arrow"
+            onClick={onClickFunction}
+            fill={color}
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z"
+                fill={color}
+            />
         </svg>
     )
-
 }
